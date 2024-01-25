aed8302f134de833564ea6096a46b69cbcd7020c
Jonathan Aires <airesjonathan123@gmail.com>
2024-01-25 20:43:51 -0300

    Service layer, component registration

--- src/main/java/com/jonaires/course/repositories/UserRepository.java
+++ src/main/java/com/jonaires/course/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jonaires.course.entities.User;
 
-public interface UserRepository extends JpaRepository<User, Long>{
+
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
---
+++ src/main/java/com/jonaires/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.jonaires.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jonaires.course.entities.User;
+import com.jonaires.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository; 	
+	
+	public List<User> findAll(){
+		return userRepository.findAll();
+	}
+	
+	public User findById(Long Id) {
+		Optional<User> obj = userRepository.findById(Id);
+		return obj.get();
+	}
+}
