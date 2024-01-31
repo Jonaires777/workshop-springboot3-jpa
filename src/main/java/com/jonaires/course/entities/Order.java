d313d07e6a2c471f6854b00e1d2debc5ae6a9de0
Jonathan Aires <airesjonathan123@gmail.com>
2024-01-31 14:32:48 -0300

    Subtotal e total methods

--- src/main/java/com/jonaires/course/entities/Order.java
+++ src/main/java/com/jonaires/course/entities/Order.java
@@ -80,7 +80,7 @@ public class Order implements Serializable {
 	public void setClient(User client) {
 		this.client = client;
 	}
-
+	
 	public Payment getPayment() {
 		return payment;
 	}
@@ -102,7 +102,15 @@ public class Order implements Serializable {
 	public Set<OrderItem> getItems() {
 		return items;
 	}
-
+	
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrderItem x : items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
