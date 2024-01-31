5e40c271d8832a4e96ec6afd2c81c67ed23e8c79
Jonathan Aires <airesjonathan123@gmail.com>
2024-01-31 19:58:17 -0300

    Exception handling - findById

--- src/main/java/com/jonaires/course/services/UserService.java
+++ src/main/java/com/jonaires/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.jonaires.course.entities.User;
 import com.jonaires.course.repositories.UserRepository;
+import com.jonaires.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long Id) {
 		Optional<User> obj = userRepository.findById(Id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(Id));
 	}
 	
 	public User insert(User obj) {
---
+++ src/main/java/com/jonaires/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,13 @@
+package com.jonaires.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. Id " + id);
+	}
+}
