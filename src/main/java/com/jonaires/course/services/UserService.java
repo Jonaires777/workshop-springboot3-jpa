38d5b39d7877ead6fd4b6d18423e3eb25018508b
Jonathan Aires <airesjonathan123@gmail.com>
2024-01-31 21:22:05 -0300

    Exception handling - update

--- src/main/java/com/jonaires/course/services/UserService.java
+++ src/main/java/com/jonaires/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.jonaires.course.repositories.UserRepository;
 import com.jonaires.course.services.exceptions.DatabaseException;
 import com.jonaires.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -44,9 +46,14 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = userRepository.getReferenceById(id);
-		updateData(entity, obj);
-		return userRepository.save(entity);
+		try {
+			User entity = userRepository.getReferenceById(id);
+			updateData(entity, obj);
+			return userRepository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		
 	}
 
 	private void updateData(User entity, User obj) {
