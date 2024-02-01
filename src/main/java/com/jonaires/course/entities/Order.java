061082ece7ceb431920e2a13da025bfd9e0bf448
Jonathan Aires <airesjonathan123@gmail.com>
2024-01-31 21:17:11 -0300

    Exception handling - delete

--- src/main/java/com/jonaires/course/entities/Order.java
+++ src/main/java/com/jonaires/course/entities/Order.java
@@ -7,7 +7,6 @@ import java.util.Objects;
 import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.jonaires.course.entities.enums.OrderStatus;
 
 import jakarta.persistence.CascadeType;
